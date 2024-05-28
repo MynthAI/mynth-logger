0d470160a0b4f01295ec76214ef1c87810c3101e
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2024-05-28 16:59:35 +0100

    Using the new modules

--- tests/app.ts
+++ tests/app.ts
@@ -1,90 +1,12 @@
-import { createConsola, LogObject, LogLevel } from "consola";
-import { format } from "src/format.js";
-import { type } from "arktype";
-import axios from "axios";
-import axiosRetry, { exponentialDelay } from "axios-retry";
-
-const Discord = type({
-  discord: type("boolean").narrow((v) => v),
-  color: "string",
-  title: "string",
-  webhookUrl: "string",
-});
-
-type Discord = typeof Discord.infer;
-
-const NullDiscord = { discord: false } as const;
-type NullDiscord = typeof NullDiscord;
-
-const getDiscord = (args: unknown[]): [Discord | NullDiscord, unknown[]] => {
-  for (let i = 0; i < args.length; i++) {
-    const discord = Discord(args[i]);
-    if (discord instanceof type.errors) continue;
-
-    return [discord, args.filter((_, j) => j !== i)];
-  }
-
-  return [NullDiscord, args];
-};
-
-const levelMap: Record<LogLevel, string> = {
-  0: "error",
-  1: "warn",
-  2: "info",
-  3: "info",
-  4: "debug",
-  5: "debug",
-};
-
-const sendToDiscord = async (description: string, options: Discord) => {
-  const axiosInstance = axios.create();
-  axiosRetry(axiosInstance, {
-    retries: 3,
-    retryDelay: exponentialDelay,
-  });
-
-  try {
-    await axiosInstance.post(options.webhookUrl, {
-      embeds: [
-        {
-          title: options.title,
-          description,
-          color: options.color,
-        },
-      ],
-    });
-  } catch (error) {
-    console.error("Unable to send message to Discord", error);
-  }
-};
+import { createConsola } from "consola";
+import DatadogReporter from "src/reporters/datadog";
+import DiscordReporter from "src/reporters/discord";
 
 const setupLogging = (dev: boolean = false) => {
   const consola = createConsola({ fancy: true });
-  const jsonReporter = {
-    log: (logObj: LogObject) => {
-      const message = JSON.stringify({
-        level: levelMap[logObj.level] || "debug",
-        message: format(logObj.args),
-      });
-      process.stdout.write(`${message}\n`);
-    },
-  };
 
-  const discordReporter = {
-    log: (logObj: LogObject) => {
-      const [discord, args] = getDiscord(logObj.args);
-      if (!discord.discord) return;
-
-      sendToDiscord(format(args), discord);
-
-      // Filter Discord data before the other reporters
-      // process the message
-      logObj.args = args;
-    },
-  };
-
-  if (!dev) consola.setReporters([jsonReporter]);
-  consola.setReporters([discordReporter, ...consola.options.reporters]);
+  if (!dev) consola.setReporters([DatadogReporter]);
+  consola.setReporters([DiscordReporter, ...consola.options.reporters]);
 
   consola.wrapConsole();
   return consola;
