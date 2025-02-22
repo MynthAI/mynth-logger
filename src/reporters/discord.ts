4b22bc7e2083c0ea44c7f7e3d58a9e1e28133fd4
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2025-02-22 10:25:17 +0100

    Revert "Allow Discord webhook URL to be set upon initialization"
    This reverts commit 4fa3fde50faae5a3da4fdbd04aa82b77b7851d8c.

--- src/reporters/discord.ts
+++ src/reporters/discord.ts
@@ -3,14 +3,11 @@ import { format } from "../format.js";
 import { type } from "arktype";
 import got from "got";
 
-const DiscordWebhookUrl = type("string.url");
-type DiscordWebhookUrl = typeof DiscordWebhookUrl.infer;
-
 const Discord = type({
   discord: type("boolean").narrow((v) => v),
   color: "string",
   title: "string",
-  "webhookUrl?": DiscordWebhookUrl,
+  webhookUrl: "string",
 });
 
 type Discord = typeof Discord.infer;
@@ -19,30 +16,17 @@ const NullDiscord = { discord: false } as const;
 
 type NullDiscord = typeof NullDiscord;
 
-const Reporter = ($discordWebhookUrl?: DiscordWebhookUrl) => {
-  if ($discordWebhookUrl) DiscordWebhookUrl.assert($discordWebhookUrl);
+const Reporter = {
+  log: (logObj: LogObject) => {
+    const [discord, args] = getDiscord(logObj.args);
+    if (!discord.discord) return;
 
-  return {
-    log: (logObj: LogObject) => {
-      const [discord, args] = getDiscord(logObj.args);
-      if (!discord.discord) return;
+    sendToDiscord(format(args), discord);
 
-      const discordWebhookUrl = discord.webhookUrl || $discordWebhookUrl;
-
-      if (!discordWebhookUrl) {
-        console.error(
-          "Unable to send message to Discord because webhook URL is missing"
-        );
-        return;
-      }
-
-      sendToDiscord(format(args), discord, discordWebhookUrl);
-
-      // Filter Discord data before the other reporters
-      // process the message
-      logObj.args = args;
-    },
-  };
+    // Filter Discord data before the other reporters
+    // process the message
+    logObj.args = args;
+  },
 };
 
 const getDiscord = (args: unknown[]): [Discord | NullDiscord, unknown[]] => {
@@ -56,11 +40,7 @@ const getDiscord = (args: unknown[]): [Discord | NullDiscord, unknown[]] => {
   return [NullDiscord, args];
 };
 
-const sendToDiscord = async (
-  description: string,
-  options: Discord,
-  webhookUrl: DiscordWebhookUrl
-) => {
+const sendToDiscord = async (description: string, options: Discord) => {
   const data = {
     json: {
       embeds: [
@@ -75,11 +55,10 @@ const sendToDiscord = async (
   };
 
   try {
-    await got.post(webhookUrl, data);
+    await got.post(options.webhookUrl, data);
   } catch (error) {
     console.error("Unable to send message to Discord", error);
   }
 };
 
 export default Reporter;
-export { DiscordWebhookUrl };
