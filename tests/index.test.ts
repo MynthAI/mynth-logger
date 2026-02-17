097111b45cf2a3178c54da3b91337d70bec82e97
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2026-02-17 22:19:52 +0100

    Added test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -18,4 +18,11 @@ describe("logging", () => {
     console.debug(false, true, "true", "false");
     expect(true).toBe(true);
   });
+
+  it("redacts private key", () => {
+    console.info(
+      "a723cc20646a45cccd045b4bd13c0f73e505e6fc7f0c5006ab62e0410a2ef9ba",
+    );
+    expect(true).toBe(true);
+  });
 });
